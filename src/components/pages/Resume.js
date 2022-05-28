171e1b8967c43525dfc36699408607b954219424
S.Snavely <info@stevesnavely.com>
2022-05-28 05:47:05 -0400

    Updated code

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React from "react";
+import resumeData from "../../resumeData";
 
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume Page</h1>
-      <p>
-      <a href="../../../SteveSnavelyResume.docx" download> Download Resume</a>
-      </p>
-    </div>
-  );
+    <div className="card">
+      <h1>Resume:</h1>
+      <p>This is my resume</p>
+    
+      <div className="card-body">
+        {resumeData.map((resumeData) => (
+          <div className="card-title">
+            <div>
+              <p>
+                <img src={`${resumeData.contact.image} `} alt={`${resumeData.contact.imageAlt} `}></img>
+              </p>
+              <p> {`${resumeData.contact.location}`}</p>
+              <p> {`${resumeData.contact.email}`}</p>
+              <p>
+                {`${resumeData.contact.linkedin.icon}`}
+                {`${resumeData.contact.linkedin.link}`}
+              </p>
+              <p>
+                {`${resumeData.contact.github.icon}`}
+                {`${resumeData.contact.github.link}`}
+              </p>
+            </div>
+            <div className="card-body">
+              <p> {`${resumeData.summary}`}</p>
+            </div>
+
+            <div className="card-body">
+            
+              <p>{`${resumeData.experience}`}</p>
+            
+            </div>
+            <div className="card-body">
+              <p>{`${resumeData.skills}`}</p>
+            </div>
+            <div className="card-body">
+              <p> {`${resumeData.core}`}</p>
+            </div>
+            <div className="card-body">
+              <p> {`${resumeData.education}`}</p>
+            </div>
+          </div>
+          
+      
+    
+  ))}
+  </div>
+  </div>)
 }
+  
+
+
--- src/resumeData.js
+++ src/resumeData.js
@@ -2,6 +2,7 @@ const resumeData = [
   {
     contact: {
       image: "../assets/me.jpg",
+      imageAlt: "Steve Snavely",
       location: "Marietta, Ga",
       email: "info@stevesnavely.com",
       linkedin: {
@@ -13,69 +14,94 @@ const resumeData = [
         link: "https://github.com/JervanSahd",
       },
     },
-    summary: "",
-    experience: [{
-      company: "Roswell Medical Group",
-      position: "Manager",
-      skills: "practice management system, electronic health record, marketing and web developement, bookkeeping",
-      compentencies:"leadership, customer service, adaptable, communitcation,    ",
-      startDate: "",
-      endDate: "",
-      duties: {
-        duty1: "",
-        duty2: "",
-        duty3: "",
+    summary:
+      "Returning worker transitioning to full stack development providing problem solving, leadership, entrepreneurial, creative thinking and customer experience. A current boot camp certificate and a Bachelors IT degree prepared a skillset geared toward front end and back end development.",
+    experience: [
+       {
+        company: "Roswell Medical Group",
+        position: "Manager",
+        skills:
+          "practice management system, electronic health record, marketing and web developement, bookkeeping",
+        compentencies:
+          "leadership, customer service, adaptable, communitcation ",
+        startDate: "January 1, 2016",
+        endDate: "April 1, 2019",
+        length: "3.5 years",
+        duties: {
+          duty1:
+            "Established and documented goals, objectives, and policies adhering to compliance standards.",
+          duty2:
+            "Administered $600,000 annual budget and supervised 15 employees and providers.",
+          duty3:
+            "Managed company data to insure compliance through electronic health record system.",
+        },
       },
-    },
-    {
-        company: "",
-        position: "",
-        skills: "",
+      {
+        company: "WebMD",
+        position: "Web Publisher",
+        skills: "html , css, photoshop",
         startDate: "",
         endDate: "",
+        length: "1 year",
         duties: {
-          duty1: "",
-          duty2: "",
+          duty1: "Maintained video and metadata for assets published on video platforms and distribution channels. ",
+          duty2: "Update and document website content with HTML, CSS, and Photoshop skills.",
           duty3: "",
         },
       },
       {
-        company: "",
-        position: "",
-        skills: "",
+        company: "Tech Data",
+        position: "New Accounts Rep/ Associate Program Manager",
+        skills: "html, css, content management systems, Siebel CRM",
         startDate: "",
         endDate: "",
+        length: "4 years",
         duties: {
-          duty1: "",
-          duty2: "",
-          duty3: "",
-        },
+          duty1: "Assist with the development of web marketing strategies in order to drive supplier and reseller relations.",
+          duty2: "Provide conference support",
+          duty3: "Continually collaborates with internal peers, and/or suppliers to effectively define, develop and execute marketing strategies.",
+        }
       },
     ],
     skills: {
       technologies: {
-          current:{
-
-          },
-          future:{
-
-          },
+        current: "HTML, CSS, Javascript, jQuery, Bootstrap, NodeJs, Express, SQL, MongoDB, multiple NPM packages, web authintication, React",
+        future: "Develope skills in all areas of Web3 technologies",
       },
     },
     core: {
-      comp1:    "Experience with end-to-end release and deployment of web applications.",
-      comp2:    "Eager; curious and passionate to learn about technology and processes.",
-      comp3:    "Collaborative; values team success more than individual accomplishments.",
-      comp4:    "Passion for knowledge; a desire to learn and share knowledge.",
-      comp5:    "Problem-solver; always ready to dive in and tackle problems head on.",
-      comp6:    "Strive to create and maintain clean code and engage in peer code reviews",
-      comp7:    "Exceptional eye for detail and passion for quality",
-      comp8:    "Inquisitive; not afraid to ask questions.",
+      comp1:
+        "Experience with end-to-end release and deployment of web applications.",
+      comp2:
+        "Eager; curious and passionate to learn about technology and processes.",
+      comp3:
+        "Collaborative; values team success more than individual accomplishments.",
+      comp4: "Passion for knowledge; a desire to learn and share knowledge.",
+      comp5:
+        "Problem-solver; always ready to dive in and tackle problems head on.",
+      comp6:
+        "Strive to create and maintain clean code and engage in peer code reviews",
+      comp7: "Exceptional eye for detail and passion for quality",
+      comp8: "Inquisitive; not afraid to ask questions.",
     },
 
     education: {
-      degree: "Github Repo",
-      certificate: "Deployed site",
+      degree: {
+          type: "Bachelor of Science in Information Technology",
+          school: "American Intercontental University",
+          icon: "",
+          description: "",
+          studies: "Computer and network Hardware, Operating systems, fundamentals of programming and Logic, Network infrastructure basics, Security",
+
+      },
+      certs: {
+        type: "Full Stack Web Development certificate",
+        school: "Georgia Tech",
+        icon: "",
+        description: "",
+        studies: "HTML, CSS, Javascript, jQuery, Bootstrap, NodeJs, Express, SQL, MongoDB, React ",
+
+    },
     },
   },
 ];

f50563c6ba050fca26e1064817d85085d9cf361e
S.Snavely <info@stevesnavely.com>
2022-05-28 07:16:22 -0400

    Updated Resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,18 +1,23 @@
 import React from "react";
 import resumeData from "../../resumeData";
 
+
 export default function Resume() {
   return (
-    <div className="card">
-      <h1>Resume:</h1>
+    <div className="container">
+      
+      <div className="jumbotron"><h1>Resume:</h1>
       <p>This is my resume</p>
-    
-      <div className="card-body">
+      </div>
+      <div className="card-body container">
         {resumeData.map((resumeData) => (
           <div className="card-title">
             <div>
               <p>
-                <img src={`${resumeData.contact.image} `} alt={`${resumeData.contact.imageAlt} `}></img>
+                <img
+                  src={`${resumeData.contact.image} `}
+                  alt={`${resumeData.contact.imageAlt} `}
+                ></img>
               </p>
               <p> {`${resumeData.contact.location}`}</p>
               <p> {`${resumeData.contact.email}`}</p>
@@ -25,32 +30,85 @@ export default function Resume() {
                 {`${resumeData.contact.github.link}`}
               </p>
             </div>
-            <div className="card-body">
+            <div className="card-body container">
               <p> {`${resumeData.summary}`}</p>
             </div>
 
-            <div className="card-body">
-            
-              <p>{`${resumeData.experience}`}</p>
-            
+            <div className="card-body container " >
+              <h2>Experience</h2>
+              <p>
+                  Company:{`${resumeData.experience1.company}`}
+                  Position:{`${resumeData.experience1.position}`}
+                  Skills:{`${resumeData.experience1.skills}`}
+                  Core Compentencies: {`${resumeData.experience1.compentencies}`}
+                  Start Date: {`${resumeData.experience1.startDate}`}
+                  End Date: {`${resumeData.experience1.endDate}`}
+                  Length: {`${resumeData.experience1.length}`}
+                  Dutie:<ul><li>{`${resumeData.experience1.duty1}`}</li>
+                  <li>{`${resumeData.experience1.duty2}`}</li>
+                  <li>{`${resumeData.experience1.duty3}`} </li>
+                  </ul>
+                </p>
+                <p>
+                  Company:{`${resumeData.experience2.company}`}
+                  Position:{`${resumeData.experience2.position}`}
+                  Skills:{`${resumeData.experience2.skills}`}
+                  Core Compentencies: {`${resumeData.experience2.compentencies}`}
+                  Start Date: {`${resumeData.experience2.startDate}`}
+                  End Date: {`${resumeData.experience2.endDate}`}
+                  Length: {`${resumeData.experience2.length}`}
+                  Dutie:<ul><li>{`${resumeData.experience2.duty1}`}</li>
+                  <li>{`${resumeData.experience2.duty2}`}</li>
+                  <li>{`${resumeData.experience2.duty3}`} </li>
+                  </ul>
+                </p>
+                <p>
+                  Company:{`${resumeData.experience3.company}`}
+                  Position:{`${resumeData.experience3.position}`}
+                  Skills:{`${resumeData.experience3.skills}`}
+                  Core Compentencies: {`${resumeData.experience3.compentencies}`}
+                  Start Date: {`${resumeData.experience3.startDate}`}
+                  End Date: {`${resumeData.experience3.endDate}`}
+                  Length: {`${resumeData.experience3.length}`}
+                  Dutie:<ul><li>{`${resumeData.experience3.duty1}`}</li>
+                  <li>{`${resumeData.experience3.duty2}`}</li>
+                  <li>{`${resumeData.experience3.duty3}`} </li>
+                  </ul>
+                </p>
             </div>
-            <div className="card-body">
-              <p>{`${resumeData.skills}`}</p>
+            <div className="card-body container">
+              <h2>Skills</h2>
+              <p>{`${resumeData.skills.current}`}
+              {`${resumeData.skills.future}`}</p>
             </div>
-            <div className="card-body">
-              <p> {`${resumeData.core}`}</p>
+            <div className="card-body container">
+              <h2>Core Compentencies</h2>
+              <p> {`${resumeData.core.comp1}`}</p>
+              <p> {`${resumeData.core.comp2}`}</p>
+              <p> {`${resumeData.core.comp3}`}</p>
+              <p> {`${resumeData.core.comp4}`}</p>
+              <p> {`${resumeData.core.comp5}`}</p>
+              <p> {`${resumeData.core.comp6}`}</p>
+              <p> {`${resumeData.core.comp7}`}</p>
+              <p> {`${resumeData.core.comp8}`}</p>
             </div>
-            <div className="card-body">
-              <p> {`${resumeData.education}`}</p>
+            <div className="card-body container">
+            <h2>Education</h2>
+            <p>
+              <h3> {`${resumeData.education.degree.type}`}</h3>
+              <p> {`${resumeData.education.degree.school}`}</p>
+              <p> {`${resumeData.education.degree.studies}`}</p>
+            </p>
+            <p>
+              <h3> {`${resumeData.education.certs.type}`}</h3>
+              <p> {`${resumeData.education.certs.school}`}</p>
+              <p> {`${resumeData.education.certs.studies}`}</p>
+            </p>
+        
             </div>
           </div>
-          
-      
-    
-  ))}
-  </div>
-  </div>)
+        ))}
+      </div>
+    </div>
+  );
 }
-  
-
-
--- src/resumeData.js
+++ src/resumeData.js
@@ -16,58 +16,48 @@ const resumeData = [
     },
     summary:
       "Returning worker transitioning to full stack development providing problem solving, leadership, entrepreneurial, creative thinking and customer experience. A current boot camp certificate and a Bachelors IT degree prepared a skillset geared toward front end and back end development.",
-    experience: [
+    experience1: 
        {
         company: "Roswell Medical Group",
         position: "Manager",
-        skills:
-          "practice management system, electronic health record, marketing and web developement, bookkeeping",
-        compentencies:
-          "leadership, customer service, adaptable, communitcation ",
+        skills: "practice management system, electronic health record, marketing and web developement, bookkeeping",
+        compentencies:"leadership, customer service, adaptable, communitcation ",
         startDate: "January 1, 2016",
         endDate: "April 1, 2019",
         length: "3.5 years",
-        duties: {
-          duty1:
-            "Established and documented goals, objectives, and policies adhering to compliance standards.",
-          duty2:
-            "Administered $600,000 annual budget and supervised 15 employees and providers.",
-          duty3:
-            "Managed company data to insure compliance through electronic health record system.",
-        },
+        duty1:"Established and documented goals, objectives, and policies adhering to compliance standards.",
+        duty2:"Administered $600,000 annual budget and supervised 15 employees and providers.",
+        duty3: "Managed company data to insure compliance through electronic health record system.",
       },
-      {
+      experience2: {
         company: "WebMD",
         position: "Web Publisher",
         skills: "html , css, photoshop",
+        compentencies:"leadership, customer service, adaptable, communitcation ",
         startDate: "",
         endDate: "",
         length: "1 year",
-        duties: {
-          duty1: "Maintained video and metadata for assets published on video platforms and distribution channels. ",
-          duty2: "Update and document website content with HTML, CSS, and Photoshop skills.",
-          duty3: "",
-        },
+        duty1: "Maintained video and metadata for assets published on video platforms and distribution channels. ",
+        duty2: "Update and document website content with HTML, CSS, and Photoshop skills.",
+        duty3: "",
       },
-      {
+      experience3: {
         company: "Tech Data",
         position: "New Accounts Rep/ Associate Program Manager",
         skills: "html, css, content management systems, Siebel CRM",
+        compentencies:"leadership, customer service, adaptable, communitcation ",
         startDate: "",
         endDate: "",
         length: "4 years",
-        duties: {
-          duty1: "Assist with the development of web marketing strategies in order to drive supplier and reseller relations.",
-          duty2: "Provide conference support",
-          duty3: "Continually collaborates with internal peers, and/or suppliers to effectively define, develop and execute marketing strategies.",
-        }
+        duty1: "Assist with the development of web marketing strategies in order to drive supplier and reseller relations.",
+        duty2: "Provide conference support",
+        duty3: "Continually collaborates with internal peers, and/or suppliers to effectively define, develop and execute marketing strategies.",
+  
       },
-    ],
+    
     skills: {
-      technologies: {
         current: "HTML, CSS, Javascript, jQuery, Bootstrap, NodeJs, Express, SQL, MongoDB, multiple NPM packages, web authintication, React",
-        future: "Develope skills in all areas of Web3 technologies",
-      },
+        future: "Develope skills in all areas of Web3 technologies"
     },
     core: {
       comp1:
